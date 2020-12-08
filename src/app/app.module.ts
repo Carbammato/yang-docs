ecff7e40a2ccaa571fb6148ddf2050e88045b025
carbammato <demetrio.marino1985@gmail.com>
2020-12-08 12:05:56 +0100

    removed routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { YangFuriganaComponent } from './yang-furigana/yang-furigana.component';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,7 +10,6 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
